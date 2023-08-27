759c3602475d9278340d22169c24b1ddd7767f47
Benedikt <benedikt.fesl@tum.de>
2023-08-27 18:23:40 +0200

    added structured GMM version with examples; speed-ups via FFT for (block-)circulant versions

--- examples.py
+++ examples.py
@@ -1,6 +1,7 @@
 import time
 import numpy as np
 from gmm_estimator import GmmEstimator
+import random
 
 
 def mse(x, y):
@@ -22,6 +23,7 @@ def example1():
     """
     Full covariance matrices with A = I.
     """
+    print('Full covariance matrices with A = I.')
     rng = np.random.default_rng(1235428719812346)
 
     n_train = 1_000
@@ -85,11 +87,10 @@ def example1():
 
 
 def example2():
-    import random
-
     """
     Full covariance matrices with selection matrix A.
     """
+    print('Full covariance matrices with selection matrix A.')
     rng = np.random.default_rng(1235428719812346)
 
     n_train = 1_000
@@ -159,6 +160,348 @@ def example2():
     return 2
 
 
+def example3():
+    """
+    Circulant covariance matrices with A = I.
+    """
+    print('Circulant covariance matrices with A = I.')
+    rng = np.random.default_rng(1235428719812346)
+
+    n_train = 1_000
+    n_eval = 100
+    n_dim = 10
+
+    #
+    # training data
+    #
+    h_train = standard_normal_cplx(n_train, n_dim, rng)
+
+    #
+    # train GMM estimator
+    #
+    tic = time.time()
+
+    gmm_estimator = GmmEstimator(
+        n_components=16,
+        random_state=2,
+        max_iter=100,
+        n_init=1,
+        covariance_type="circulant",
+    )
+    gmm_estimator.fit(h_train)
+
+    toc = time.time()
+    print(f"training done. ({toc - tic:.3f} s)")
+
+    #
+    # data to evaluate the GMM estimator
+    #
+    h_eval = standard_normal_cplx(n_eval, n_dim, rng)
+    noise_eval = standard_normal_cplx(n_eval, n_dim, rng)
+    noise_covariance = np.eye(n_dim, dtype=complex)
+    # noisy observations, y = h + n
+    y_eval = h_eval + noise_eval
+
+    #
+    # evaluate GMM estimator
+    #
+    tic = time.time()
+
+    # use all components for the evaluation
+    h_est = gmm_estimator.estimate(
+        y_eval, noise_covariance, n_dim, n_components_or_probability=1.0
+    )
+    print("MSE with n_components_or_probability=1.0:", mse(h_est, h_eval))
+    del h_est
+
+    # use the top 3 components for the evaluation
+    h_est = gmm_estimator.estimate(
+        y_eval, noise_covariance, n_dim, n_components_or_probability=3
+    )
+    print("MSE with n_components_or_probability=3:", mse(h_est, h_eval))
+    del h_est
+
+    toc = time.time()
+    print(f"estimation done. ({toc - tic:.3f} s)")
+
+    return 3
+
+
+def example4():
+    """
+    Circulant covariance matrices with selection matrix A.
+    """
+    print('Circulant covariance matrices with selection matrix A.')
+    rng = np.random.default_rng(1235428719812346)
+
+    n_train = 1_000
+    n_eval = 100
+    n_dim = 10
+    n_dim_obs = 5
+
+    #
+    # create a random selection matrix
+    #
+    A = np.zeros([n_dim_obs, n_dim])
+    pattern_vec = random.sample(range(n_dim), n_dim_obs)
+    pattern_vec.sort()
+    for i, val in enumerate(pattern_vec):
+        A[i, val] = 1
+
+    #
+    # training data
+    #
+    h_train = standard_normal_cplx(n_train, n_dim, rng)
+
+    #
+    # train GMM estimator
+    #
+    tic = time.time()
+
+    gmm_estimator = GmmEstimator(
+        n_components=16,
+        random_state=2,
+        max_iter=100,
+        n_init=1,
+        covariance_type="circulant",
+    )
+    gmm_estimator.fit(h_train)
+    toc = time.time()
+    print(f"training done. ({toc - tic:.3f} s)")
+
+    #
+    # data to evaluate the GMM estimator
+    #
+    h_eval = standard_normal_cplx(n_eval, n_dim, rng)
+    noise_eval = standard_normal_cplx(n_eval, n_dim_obs, rng)
+    noise_covariance = np.eye(n_dim_obs, dtype=complex)
+    # noisy observations, y = A h + n
+    y_eval = np.squeeze(np.matmul(A, np.expand_dims(h_eval, 2))) + noise_eval
+
+    #
+    # GMM evaluation
+    #
+    tic = time.time()
+
+    h_est = gmm_estimator.estimate(
+        y_eval, noise_covariance, n_dim, A=A, n_components_or_probability=1.0
+    )
+    print("MSE with n_components_or_probability=1.0:", mse(h_est, h_eval))
+    del h_est
+
+    h_est = gmm_estimator.estimate(
+        y_eval, noise_covariance, n_dim, A=A, n_components_or_probability=3
+    )
+    print("MSE with n_components_or_probability=3:", mse(h_est, h_eval))
+    del h_est
+
+    toc = time.time()
+    print(f"estimation done. ({toc - tic:.3f} s)")
+
+    return 4
+
+
+def example5():
+    """
+    Block-circulant covariance matrices with A = I.
+    """
+    print('Block-circulant covariance matrices with A = I.')
+    rng = np.random.default_rng(1235428719812346)
+
+    n_train = 1_000
+    n_eval = 100
+    n_dim = 12
+
+    #
+    # training data
+    #
+    h_train = standard_normal_cplx(n_train, n_dim, rng)
+
+    #
+    # train GMM estimator
+    #
+    tic = time.time()
+
+    gmm_estimator = GmmEstimator(
+        n_components=16,
+        random_state=2,
+        max_iter=100,
+        n_init=1,
+        covariance_type="block-circulant",
+    )
+    gmm_estimator.fit(h_train, blocks=(4, 3))
+
+    toc = time.time()
+    print(f"training done. ({toc - tic:.3f} s)")
+
+    #
+    # data to evaluate the GMM estimator
+    #
+    h_eval = standard_normal_cplx(n_eval, n_dim, rng)
+    noise_eval = standard_normal_cplx(n_eval, n_dim, rng)
+    noise_covariance = np.eye(n_dim, dtype=complex)
+    # noisy observations, y = h + n
+    y_eval = h_eval + noise_eval
+
+    #
+    # evaluate GMM estimator
+    #
+    tic = time.time()
+
+    # use all components for the evaluation
+    h_est = gmm_estimator.estimate(
+        y_eval, noise_covariance, n_dim, n_components_or_probability=1.0
+    )
+    print("MSE with n_components_or_probability=1.0:", mse(h_est, h_eval))
+    del h_est
+
+    # use the top 3 components for the evaluation
+    h_est = gmm_estimator.estimate(
+        y_eval, noise_covariance, n_dim, n_components_or_probability=3
+    )
+    print("MSE with n_components_or_probability=3:", mse(h_est, h_eval))
+    del h_est
+
+    toc = time.time()
+    print(f"estimation done. ({toc - tic:.3f} s)")
+
+    return 5
+
+
+def example6():
+    """
+    Toeplitz covariance matrices with A = I.
+    """
+    print('Toeplitz covariance matrices with A = I.')
+    rng = np.random.default_rng(1235428719812346)
+
+    n_train = 1_000
+    n_eval = 100
+    n_dim = 10
+
+    #
+    # training data
+    #
+    h_train = standard_normal_cplx(n_train, n_dim, rng)
+
+    #
+    # train GMM estimator
+    #
+    tic = time.time()
+
+    gmm_estimator = GmmEstimator(
+        n_components=16,
+        random_state=2,
+        max_iter=100,
+        n_init=1,
+        covariance_type="toeplitz",
+    )
+    gmm_estimator.fit(h_train)
+
+    toc = time.time()
+    print(f"training done. ({toc - tic:.3f} s)")
+
+    #
+    # data to evaluate the GMM estimator
+    #
+    h_eval = standard_normal_cplx(n_eval, n_dim, rng)
+    noise_eval = standard_normal_cplx(n_eval, n_dim, rng)
+    noise_covariance = np.eye(n_dim, dtype=complex)
+    # noisy observations, y = h + n
+    y_eval = h_eval + noise_eval
+
+    #
+    # evaluate GMM estimator
+    #
+    tic = time.time()
+
+    # use all components for the evaluation
+    h_est = gmm_estimator.estimate(
+        y_eval, noise_covariance, n_dim, n_components_or_probability=1.0
+    )
+    print("MSE with n_components_or_probability=1.0:", mse(h_est, h_eval))
+    del h_est
+
+    # use the top 3 components for the evaluation
+    h_est = gmm_estimator.estimate(
+        y_eval, noise_covariance, n_dim, n_components_or_probability=3
+    )
+    print("MSE with n_components_or_probability=3:", mse(h_est, h_eval))
+    del h_est
+
+    toc = time.time()
+    print(f"estimation done. ({toc - tic:.3f} s)")
+
+    return 6
+
+
+def example7():
+    """
+    Block-Toeplitz covariance matrices with A = I.
+    """
+    print('Block-Toeplitz covariance matrices with A = I.')
+    rng = np.random.default_rng(1235428719812346)
+
+    n_train = 1_000
+    n_eval = 100
+    n_dim = 12
+
+    #
+    # training data
+    #
+    h_train = standard_normal_cplx(n_train, n_dim, rng)
+
+    #
+    # train GMM estimator
+    #
+    tic = time.time()
+
+    gmm_estimator = GmmEstimator(
+        n_components=16,
+        random_state=2,
+        max_iter=200,
+        n_init=1,
+        covariance_type="block-toeplitz",
+    )
+    gmm_estimator.fit(h_train, blocks=(3, 4))
+
+    toc = time.time()
+    print(f"training done. ({toc - tic:.3f} s)")
+
+    #
+    # data to evaluate the GMM estimator
+    #
+    h_eval = standard_normal_cplx(n_eval, n_dim, rng)
+    noise_eval = standard_normal_cplx(n_eval, n_dim, rng)
+    noise_covariance = np.eye(n_dim, dtype=complex)
+    # noisy observations, y = h + n
+    y_eval = h_eval + noise_eval
+
+    #
+    # evaluate GMM estimator
+    #
+    tic = time.time()
+
+    # use all components for the evaluation
+    h_est = gmm_estimator.estimate(
+        y_eval, noise_covariance, n_dim, n_components_or_probability=1.0
+    )
+    print("MSE with n_components_or_probability=1.0:", mse(h_est, h_eval))
+    del h_est
+
+    # use the top 3 components for the evaluation
+    h_est = gmm_estimator.estimate(
+        y_eval, noise_covariance, n_dim, n_components_or_probability=3
+    )
+    print("MSE with n_components_or_probability=3:", mse(h_est, h_eval))
+    del h_est
+
+    toc = time.time()
+    print(f"estimation done. ({toc - tic:.3f} s)")
+
+    return 7
+
+
 if __name__ == "__main__":
     import argparse
 
@@ -177,7 +520,7 @@ if __name__ == "__main__":
         eval(f"example{parargs.nr}()")
     else:
         # run all examples
-        for nr in [1, 2]:
+        for nr in range(1, 8):
             if nr > 1:
                 print()
             print(f"Running example {nr}.")
--- gmm_estimator.py
+++ gmm_estimator.py
@@ -45,12 +45,12 @@ class GmmEstimator(gmm_cplx.GaussianMixtureCplx):
                 # use predicted label to choose the channel covariance matrix
                 labels = self._predict_cplx(y_for_prediction)
                 for yi in range(y.shape[0]):
-                    mean_h = self.means_cplx[labels[yi], :]
+                    mean_h = self.means_cplx[labels[yi]]
                     h_est[yi, :] = self._lmmse_formula(
-                        y[yi, :],
+                        y_for_prediction[yi, :],
                         mean_h,
-                        self.covs_cplx[labels[yi], :, :] @ A.conj().T,
-                        covs_Cy_inv[labels[yi], :, :],
+                        self.covs_cplx[labels[yi]] @ A.conj().T,
+                        covs_Cy_inv[labels[yi]],
                         A @ mean_h,
                     )
             else:
@@ -62,10 +62,10 @@ class GmmEstimator(gmm_cplx.GaussianMixtureCplx):
                     for argproba in idx_sort[:n_components_or_probability]:
                         mean_h = self.means_cplx[argproba, :]
                         h_est[yi, :] += proba[yi, argproba] * self._lmmse_formula(
-                            y[yi, :],
+                            y_for_prediction[yi, :],
                             mean_h,
-                            self.covs_cplx[argproba, :, :] @ A.conj().T,
-                            covs_Cy_inv[argproba, :, :],
+                            self.covs_cplx[argproba] @ A.conj().T,
+                            covs_Cy_inv[argproba],
                             A @ mean_h,
                         )
                     h_est[yi, :] /= np.sum(
@@ -78,10 +78,10 @@ class GmmEstimator(gmm_cplx.GaussianMixtureCplx):
                 for argproba in range(proba.shape[1]):
                     mean_h = self.means_cplx[argproba, :]
                     h_est[yi, :] += proba[yi, argproba] * self._lmmse_formula(
-                        y[yi, :],
+                        y_for_prediction[yi, :],
                         mean_h,
-                        self.covs_cplx[argproba, :, :] @ A.conj().T,
-                        covs_Cy_inv[argproba, :, :],
+                        self.covs_cplx[argproba] @ A.conj().T,
+                        covs_Cy_inv[argproba],
                         A @ mean_h,
                     )
         else:
@@ -100,10 +100,10 @@ class GmmEstimator(gmm_cplx.GaussianMixtureCplx):
                 for argproba in idx_sort[:nr_proba]:
                     mean_h = self.means_cplx[argproba, :]
                     h_est[yi, :] += proba[yi, argproba] * self._lmmse_formula(
-                        y[yi, :],
+                        y_for_prediction[yi, :],
                         mean_h,
-                        self.covs_cplx[argproba, :, :] @ A.conj().T,
-                        covs_Cy_inv[argproba, :, :],
+                        self.covs_cplx[argproba] @ A.conj().T,
+                        covs_Cy_inv[argproba],
                         A @ mean_h,
                     )
                 h_est[yi, :] /= np.sum(proba[yi, idx_sort[:nr_proba]])
@@ -116,34 +116,30 @@ class GmmEstimator(gmm_cplx.GaussianMixtureCplx):
         covariance matrices of the observation. Further, in case of diagonal
         matrices, FFT-transform the observation.
         """
-
-        if self.gm.covariance_type == "diag":
-            # raise error if A is not identity or quadratic matrix
-            try:
-                diff = np.sum(np.abs(A - np.eye(A.shape[0])) ** 2)
-                if diff > 1e-12:
-                    NotImplementedError(
-                        f"Estimation for covariance_type = {self.gm.covariance_type} with arbitrary matrix A is not implemented."
-                    )
-            except:
-                raise NotImplementedError(
-                    f"Estimation for covariance_type = {self.gm.covariance_type} with arbitrary matrix A is not implemented."
-                )
-
-            # update GMM covs
-            covs_gm = self.gm.covariances_.copy()
+        # check if FFT speed-ups and diagonal covs can be used for circulant covariances
+        if A.shape[0] == A.shape[1]:
+            diag_tf = np.isclose(np.sum(np.abs(A - A[0,0]*np.eye(A.shape[0])) ** 2), 0.0)
+            diag_tf = diag_tf and np.isclose(np.sum(np.abs(noise_covariance -
+                                               noise_covariance[0,0]*np.eye(noise_covariance.shape[0])) ** 2), 0.0)
+        else:
+            diag_tf = False
+        if (self.params['cov_type'] == 'circulant' or self.params['cov_type'] == 'block-circulant') and diag_tf:
+            # update GMM covs by transformation to Fourier domain where cov is diag
+            y_for_prediction = self._dft_trafo(y)
+            # update means
+            self.gm.means_ = np.diag(A) * self.means_cplx
+            # update covs
+            covs_gm = np.zeros([self.covs_cplx.shape[0], y.shape[-1]], dtype=complex)
             for i in range(covs_gm.shape[0]):
-                covs_gm[i, :] = covs_gm[i, :] + noise_covariance
+                covs_gm[i, :] = self.covs_cplx[i, :] + np.diag(noise_covariance)
             self.gm.covariances_ = covs_gm.copy()  # this has no effect
             self.gm.precisions_cholesky_ = compute_precision_cholesky(
                 covs_gm, covariance_type="diag"
             )
+            # pre-compute inverse covs of observations
+            covs_Cy_inv = 1 / covs_gm
+            self.params['dft_trafo'] = True
 
-            # FFT of observation
-            if "block-diag" in self.params:
-                y_for_prediction = np.squeeze(self.F2 @ np.expand_dims(y, 2))
-            else:
-                y_for_prediction = np.fft.fft(y, axis=1) / np.sqrt(y.shape[-1])
         elif self.gm.covariance_type == "full":
             # update GMM means
             Am = np.squeeze(np.matmul(A, np.expand_dims(self.means_cplx, axis=2)))
@@ -154,8 +150,8 @@ class GmmEstimator(gmm_cplx.GaussianMixtureCplx):
                 self.gm.means_ = Am
 
             # update GMM covs
-            covs_gm = self.covs_cplx.copy()
-            covs_gm = np.matmul(np.matmul(A, covs_gm), A.conj().T)
+            #covs_gm = self.covs_cplx.copy()
+            covs_gm = np.matmul(np.matmul(A, self.covs_cplx), A.conj().T)
             for i in range(covs_gm.shape[0]):
                 covs_gm[i, :, :] = covs_gm[i, :, :] + noise_covariance
             self.gm.covariances_ = covs_gm.copy()  # this has no effect
@@ -166,26 +162,60 @@ class GmmEstimator(gmm_cplx.GaussianMixtureCplx):
             # update GMM feature number
             self.gm.n_features_in_ = A.shape[0]
 
-            y_for_prediction = y
+            y_for_prediction = y.copy()
+            # pre-compute inverse covs of observations
+            covs_Cy_inv = np.linalg.pinv(covs_gm, hermitian=True)
         else:
             raise NotImplementedError(
                 f"Estimation for covariance_type = {self.gm.covariance_type} is not implemented."
             )
 
-        # precompute the inverse matrices
-        covs_Cy_inv = np.zeros(
-            [self.covs_cplx.shape[0], A.shape[0], A.shape[0]], dtype=complex
-        )
-        for i in range(self.covs_cplx.shape[0]):
-            covs_Cy_inv[i, :, :] = np.linalg.pinv(
-                A @ self.covs_cplx[i, :, :] @ A.conj().T + noise_covariance
-            )
-
         return y_for_prediction, covs_Cy_inv
 
-    def _lmmse_formula(self, y, mean_h, cov_h, cov_y_inv, mean_y):
-        return mean_h + cov_h @ (cov_y_inv @ (y - mean_y))
 
+    def _lmmse_formula(self, y, mean_h, cov_h, cov_y_inv, mean_y):
+        len_cy = len(cov_y_inv.shape)
+        if self.params['cov_type'] == 'circulant' and len_cy == 1:
+            hest = mean_h + cov_h * (cov_y_inv * (y - mean_y))
+            return np.fft.ifft(hest, axis=0) * np.sqrt(hest.shape[0])
+        elif self.params['cov_type'] == 'block-circulant' and len_cy == 1:
+            hest = mean_h + cov_h * (cov_y_inv * (y - mean_y))
+            return np.squeeze(self.F2.conj().T @ hest[:, None])
+        else:
+            hest = mean_h + cov_h @ (cov_y_inv @ (y - mean_y))
+            return hest
+
+
+    def _dft_trafo(self, y):
+        """
+        If not done yet, transform GMM parameters in Fourier domain via DFT (FFTs) and transform observations.
+        """
+        if not self.params['dft_trafo']:
+            self.gm.covariance_type = "diag"
+            if self.params['cov_type'] == 'circulant':
+                dft_matrix = np.fft.fft(np.eye(self.means_cplx.shape[-1], dtype=complex)) / np.sqrt(self.means_cplx.shape[-1])
+                self.gm.means_ = np.fft.fft(self.means_cplx, axis=1) / np.sqrt(self.means_cplx.shape[-1])
+                self.means_cplx = self.gm.means_.copy()
+                # diagonalize (block-)circulant cov in Fourier domain
+                self.covs_cplx = np.zeros(self.covs_cplx.shape[:-1], dtype=complex)
+                for i in range(self.covs_cplx.shape[0]):
+                    self.covs_cplx[i] = np.diag(dft_matrix @ self.gm.covariances_[i] @ dft_matrix.conj().T)
+                self.gm.covariances_ = self.covs_cplx.copy()
+            elif self.params['cov_type'] == 'block-circulant':
+                self.gm.means_ = np.squeeze(np.matmul(self.F2, np.expand_dims(self.means_cplx, axis=2)))
+                self.means_cplx = self.gm.means_.copy()
+                # diagonalize (block-)circulant cov in Fourier domain
+                self.covs_cplx = np.zeros(self.covs_cplx.shape[:-1], dtype=complex)
+                for i in range(self.covs_cplx.shape[0]):
+                    self.covs_cplx[i] = np.diag(self.F2 @ self.gm.covariances_[i] @ self.F2.conj().T)
+                self.gm.covariances_ = self.covs_cplx.copy()
+            else:
+                NotImplementedError('Not implemented!')
+        if self.params['cov_type'] == 'circulant':
+            y_for_prediction = np.fft.fft(y, axis=1) / np.sqrt(y.shape[-1])
+        else:
+            y_for_prediction = np.squeeze(self.F2 @ np.expand_dims(y, 2))
+        return y_for_prediction
 
 def compute_precision_cholesky(covariances, covariance_type):
     """Compute the Cholesky decomposition of the precisions.
@@ -230,3 +260,4 @@ def compute_precision_cholesky(covariances, covariance_type):
             raise ValueError(estimate_precision_error_message)
         precisions_chol = 1.0 / np.sqrt(covariances).conj()
     return precisions_chol
+
